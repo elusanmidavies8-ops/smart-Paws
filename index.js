89f33d0f32c176234ae3c6d89c1cfd580128e523
elusanmidavies8-ops <elusanmidavies8@gmail.com>
2026-01-11 07:45:15 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+ document.getElementById("mysubmit").onclick = function(){
+	window.alert("Account Created Successfully")
+}
+
+let username;
+document.getElementById("mysubmit").onclick = function(){
+username = document.getElementById("text").value;
+console.log(username);
+}
